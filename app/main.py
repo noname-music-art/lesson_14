b6ab46991e1d98d5c26b3ba9ed7fcd5a2e9b3b16
Konstantin <vasilevki@gmail.com>
2022-07-25 19:32:37 +0300

    steps 0..3

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from flask import Blueprint
+from app.utils import get_movie_by_title, get_movie_by_date_range, get_movie_by_rating
+
+main_blueprint = Blueprint('main_blueprint', __name__)
+
+
+@main_blueprint.route("/movies/<title>")
+def page_movies(title):
+    return get_movie_by_title(title)
+
+
+@main_blueprint.route("/movies/<start>/to/<stop>")
+def page_movies_date_ranged(start, stop):
+    return get_movie_by_date_range(start, stop)
+
+
+@main_blueprint.route("/rating/children")
+def page_children_movies():
+    return get_movie_by_rating("!", "G")
+
+
+@main_blueprint.route("/rating/family")
+def page_family_movies():
+    return get_movie_by_rating("G", "PG", "PG-13")
+
+
+@main_blueprint.route("/rating/adult")
+def page_adult_movies():
+    return get_movie_by_rating("R", "NC-17")
+
---
+++ app/utils.py
@@ -0,0 +1,41 @@
+import sqlite3
+from flask import jsonify, Response
+
+
+def get_movie_by_title(title: str) -> Response:
+
+    columns = ["title", "country", "release_year", "genre", "description"]
+    with sqlite3.connect('data/netflix.db') as connection:
+        cur = connection.cursor()
+        cur.execute("SELECT title, country, release_year, listed_in, description FROM netflix "
+                    "WHERE `title` = :title", {"title": title})
+        data = cur.fetchall()
+        zipped = dict(zip(columns, data[0]))
+        return jsonify(zipped)
+
+
+def get_movie_by_date_range(start: int, stop: int) -> Response:
+    columns = ["title", "release_year"]
+    zipped = []
+    with sqlite3.connect('data/netflix.db') as connection:
+        cur = connection.cursor()
+        cur.execute("SELECT title, release_year FROM netflix "
+                    "WHERE release_year BETWEEN :start AND :stop ", {"start": start, "stop": stop})
+        data = cur.fetchall()
+        for item in data:
+            zipped.append(dict(zip(columns, item)))
+        return jsonify(zipped)
+
+
+def get_movie_by_rating(*rating: str) -> Response:
+    columns = ["title", "rating", "description"]
+    zipped = []
+
+    with sqlite3.connect('data/netflix.db') as connection:
+        cur = connection.cursor()
+        cur.execute("SELECT title, rating, description FROM netflix "
+                    "WHERE rating IN {}".format(rating))
+        data = cur.fetchall()
+        for item in data:
+            zipped.append(dict(zip(columns, item)))
+        return jsonify(zipped)
